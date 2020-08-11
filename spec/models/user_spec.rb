6f14a0be1a33f67150109423eb79e92946e542ae
kyle-law <junwei_1121@hotmail.com>
2020-08-11 20:26:48 +0800

    Add Rspec testing for model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+RSpec.describe User, type: :model do
+  context 'User associations tests' do
+    it { should have_many(:friends)}
+    it { should have_many(:inverse_friendships).class_name('Friendship').with_foreign_key('friend_id') }
+  end
+end
