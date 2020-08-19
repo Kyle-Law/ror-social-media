246e534d731a026eb7f553f6e40a198cfb506bc0
kyle-law <junwei_1121@hotmail.com>
2020-08-19 21:41:40 +0800

    Configure JavaScript

--- app/assets/javascripts/dark-light-toggle.js
+++ app/assets/javascripts/dark-light-toggle.js
@@ -6,24 +6,19 @@ var toggleIcon = document.getElementById("toggle-icon");
 function darkMode() {
   console.log("nav", nav);
   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  // textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
   toggleIcon.children[0].textContent = "Dark Mode";
   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-  // imageMode("dark");
 }
 
 // Light Mode Styles
 function lightMode() {
   nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  // textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
   toggleIcon.children[0].textContent = "Light Mode";
   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-  // imageMode("light");
 }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
-  // console.log(event.target.checked);
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
